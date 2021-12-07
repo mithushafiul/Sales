6904cf4ff7228d83f7242e69caeb74e5bccb4557
DESKTOP-G6H8HSB\Asus <mithu.shafiul@gmail.com>
2021-12-07 14:14:41 +0600

    Submit

---
+++ Source/SALESMVC/SALESMVC/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using SALESMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SALESMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public static DataTable dtTable;
+        public static string connectionString = ConfigurationSettings.AppSettings["ConnectionString"];
+        public SqlConnection conn = new SqlConnection(connectionString);
+        public SqlDataAdapter SqlDataAdapter = new SqlDataAdapter();
+
+        SALESDBEntities db = new SALESDBEntities();
+        public ActionResult SaleList()
+        {
+            return View(db.SALES.ToList());
+        }
+       
+        public ActionResult SalePurchase(string SearchString)
+        {
+            string query = "";
+            string ITEM_NAME = SearchString;
+            DataSet ds = new DataSet();
+
+            query = ("EXEC [SALE_PURCHASE] @ITEM_NAME='" + ITEM_NAME + "' ");
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand comm = new SqlCommand(query, conn);            
+            try
+            {
+                conn.Open();
+                comm.CommandType = CommandType.Text;
+                SqlDataAdapter sda = new SqlDataAdapter(comm);
+                sda.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }            
+               
+            return View(ds);            
+        }
+
+
+        public ActionResult exportReport(string SearchString)
+        {
+            string query = "";
+            string ITEM_NAME = SearchString;
+
+            query = ("[dbo].SALE_PURCHASE @ITEM_NAME='" + ITEM_NAME + "'");
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand comm = new SqlCommand(query, conn);
+            SalePurchase sapu = new SalePurchase();               
+            DataSet ds = new DataSet();
+            try
+            {
+                conn.Open();
+                comm.CommandType = CommandType.Text;
+                SqlDataAdapter sda = new SqlDataAdapter(comm);
+                sda.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                   
+            }
+            finally
+            {
+            }           
+
+            ReportDocument rd = new ReportDocument();
+            rd.Load(Path.Combine(Server.MapPath("~/Report"), "SalePurchaseReport.rpt"));
+
+            rd.SetDataSource(ds.Tables[0]);
+
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+
+            try
+            {
+                Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                stream.Seek(0, SeekOrigin.Begin);
+                return File(stream, "application/pdf");
+            }
+
+            catch
+            {
+                throw;
+            }
+        }  
+    }
+}
---
+++ Source/SALESMVC/SALESMVC/Models/SalePurchase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SALESMVC.Models
+{
+    public class SalePurchase
+    {
+       public string  INVOICE_NO {get;set;}
+       public DateTime ENTRY_DATE { get; set; }
+       public string ITEM_NAME { get; set; }
+       public decimal SALES_PRICE { get; set; }
+       public decimal PURCHASE_PRICE { get; set; }
+       public decimal PROFIT { get; set; }
+        
+    }
+}
